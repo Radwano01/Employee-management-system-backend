d882cba18a372496c8f9c467f7b81ed15a1bdde3
Radwano01 <127989522+Radwano01@users.noreply.github.com>
2024-03-04 22:44:53 +0300

    Update EmployeeManagementApplication.java

--- src/main/java/com/example/employeemanagement/EmployeeManagementApplication.java
+++ src/main/java/com/example/employeemanagement/EmployeeManagementApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@ComponentScan({"com.example.employeemanagement.Repository", "com.example.employeemanagement.Security"})
 public class EmployeeManagementApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeManagementApplication.class, args);
