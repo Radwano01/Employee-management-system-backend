ab898736587faaeae67cba21ad649ca478e0b29c
Radwano01 <127989522+Radwano01@users.noreply.github.com>
2024-03-23 23:28:33 +0300

    Delete src/main/java/com/example/employeemanagement/Security/SecurityConstants.java

--- src/main/java/com/example/employeemanagement/Security/SecurityConstants.java
+++
@@ -1,7 +0,0 @@
-package com.example.employeemanagement.Security;
-
-public class SecurityConstants {
-
-    public static final long JWT_EXPIRATION = 7000;
-    public static final String JWT_SECRET = "jwt_secret";
-}
